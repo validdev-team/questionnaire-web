0ea42c4a6c8f45c7aa26337a37118ad3fb3230b9
desraymondz <desraymondz@gmail.com>
2025-07-22 12:56:30 +0800

    debug results be

--- src/app/api/results/route.js
+++ src/app/api/results/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../../lib/firebase';
+import { db } from '../../../../lib/firebase';
 
 export async function GET() {
     try {
