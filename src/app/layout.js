7f9e0246ea94e59c65275cef57c0adc01d0ebc50
desraymondz <desraymondz@gmail.com>
2025-07-23 11:37:28 +0800

    add inter font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,12 @@
+// app/layout.js
 import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter', // 👈 this makes --font-inter available in CSS
+})
 
 export const metadata = {
   title: 'Survey App',
@@ -7,8 +15,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex items-center justify-center bg-white">
+    <html lang="en" className={inter.variable}>
+      <body className="min-h-screen flex items-center justify-center bg-white text-[#0F0251]">
         {children}
       </body>
     </html>
