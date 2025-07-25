0f10b1c772a25461dd3b18261baa3d863521c71b
desraymondz <desraymondz@gmail.com>
2025-07-25 12:18:58 +0800

    Delete all qXcY collections on reset

--- src/app/api/admin/reset/route.js
+++ src/app/api/admin/reset/route.js
@@ -5,19 +5,38 @@ import { collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firesto
 
 export async function POST() {
     try {
-        // Delete responses
+        // Delete all docs from 'responses' collection
         const responseSnap = await getDocs(collection(db, 'responses'));
-        const deletePromises = responseSnap.docs.map(d => deleteDoc(doc(db, 'responses', d.id)));
-        await Promise.all(deletePromises);
+        const deleteResponsePromises = responseSnap.docs.map(d =>
+            deleteDoc(doc(db, 'responses', d.id))
+        );
+        await Promise.all(deleteResponsePromises);
 
-        // Reset votes in questions
+        // Reset votes in 'questions' collection
+        // TODO: no need this anymore actualy
         const questionsSnap = await getDocs(collection(db, 'questions'));
-        const resetPromises = questionsSnap.docs.map((docSnap) => {
+        const resetQuestionVotesPromises = questionsSnap.docs.map((docSnap) => {
             const question = docSnap.data();
             const resetChoices = question.choices.map(c => ({ ...c, votes: 0 }));
             return updateDoc(doc(db, 'questions', docSnap.id), { choices: resetChoices });
         });
-        await Promise.all(resetPromises);
+        await Promise.all(resetQuestionVotesPromises);
+
+        // Delete all qXcY collections
+        const subcollectionNames = [
+            'q1c1', 'q1c2', 'q1c3', 'q1c4', 'q1c5', 'q1c6', 'q1c7', 'q1c8', 'q1c9',
+            'q2c1', 'q2c2', 'q2c3', 'q2c4', 'q2c5'
+        ];
+
+        const deleteSubcollections = await Promise.all(
+            subcollectionNames.map(async (name) => {
+                const snap = await getDocs(collection(db, name));
+                const deletes = snap.docs.map(d => 
+                    deleteDoc(doc(db, name, d.id))
+                );
+                return Promise.all(deletes);
+            })
+        );
 
         return NextResponse.json({ success: true });
     } catch (err) {
